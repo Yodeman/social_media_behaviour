53de9449789a8f9393d6c700fed78d1226d2140b
Oyelabi Paul <59335237+Yodeman@users.noreply.github.com>
2021-02-15 01:14:12 +0100

    Update emotions.py

--- sentiments/emotions.py
+++ sentiments/emotions.py
@@ -7,12 +7,12 @@ import cv2
 WIDTH, HEIGHT = 250, 250
 
 #load encoder and model
-with open(r'.\encoder_model', 'rb') as f:
+with open('sentiments/encoder_model', 'rb') as f:
     encoder_model = pickle.load(f)
     encoder, model = encoder_model['encoder'], encoder_model['model']
 
 #load sentiment classifier
-with open(r"..\classifier\classifier", 'rb') as f:
+with open("./classifier/classifier", 'rb') as f:
     classifier, _, _ = pickle.load(f)
     
 def transform(comment):
